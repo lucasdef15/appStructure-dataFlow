66b7db1bc5aec8b937bb6211d410d94489306748
lucas <lucasff15@hotmail.com>
2023-04-10 19:21:35 -0300

    new file:   .gitignore 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.jsx 	new file:   src/app/store.js 	new file:   src/assets/react.svg 	new file:   src/features/posts/AddPostForm.jsx 	new file:   src/features/posts/PostAuthor.jsx 	new file:   src/features/posts/PostsList.jsx 	new file:   src/features/posts/ReactionButtons.jsx 	new file:   src/features/posts/TimeAgo.jsx 	new file:   src/features/posts/postsSlice.js 	new file:   src/features/users/usersSlice.js 	new file:   src/main.jsx 	new file:   src/styles/index.css 	new file:   vite.config.js

---
+++ src/features/posts/AddPostForm.jsx
@@ -0,0 +1,68 @@
+//just because we're using redux doesn't mean we won't use state in some components, because this is a temporary state it's just a controlled form input for title and content, and so we don't really need to send those to the global state they're just for this component, se we only want to send things to the global state that other components would possibly use throughout the aplication.
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { postAdded } from "./postsSlice";
+import { selectAllUsers } from "../users/usersSlice";
+
+export default function AddPostForm() {
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const users = useSelector(selectAllUsers);
+
+  const onTitleChange = (e) => setTitle(e.target.value);
+  const onContentChange = (e) => setContent(e.target.value);
+  const onAuthorChange = (e) => setUserId(e.target.value);
+
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postAdded(title, content, userId));
+      setContent("");
+      setTitle("");
+      setUserId("");
+    }
+  };
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+
+  const userOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
+
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitel"
+          value={title}
+          onChange={onTitleChange}
+        />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChange}>
+          <option value=""></option>
+          {userOptions}
+        </select>
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChange}
+        />
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
+    </section>
+  );
+}
